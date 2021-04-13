8d57d2b25887b45b03a5f78af2c24fad9bf355c6
David Ready <davis_ready@hotmail.com>
2021-04-13 18:54:02 -0400

    more Navbar styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Home from './Components/Home';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-   <>
+  <CssBaseline>
    <Home/>
-   </> 
+   </CssBaseline>
+   
   );
 }
 
--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,20 +1,63 @@
 
 import React from 'react'
-import { AppBar,  Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box } from '@material-ui/core';
-import { ArrowBack, AssignmentInd, Home, Apps, ContactMail } from '@material-ui/icons';
+import { AppBar,  
+         Toolbar, 
+         ListItem, 
+         IconButton, 
+         ListItemText, 
+         Avatar, 
+         Divider, 
+         List, 
+         Typography, 
+         Box 
+        } from '@material-ui/core';
+import { ArrowBack, 
+         AssignmentInd, 
+         Home, 
+         Apps, 
+         ContactMail 
+        } from '@material-ui/icons';
+import avatar from "../avatar.png"
+import { makeStyles } from '@material-ui/core/styles';
 
+
+
+//CSS STYLES
+const useStyles = makeStyles(theme=>({
+    menuSliderContainer: {
+        width: 250,
+        background: "#511",
+        height: "30rem"
+    },
+    avatar: {
+        display: "block",
+        margin: "0.5rem auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13)
+    }
+}));
 
 const Navbar = () => {
+    const classes = useStyles;
     return (
-        <Box component= "nav">
+        <>
+        <Box className={classes.menuSliderContainer} component="div">
+          <Avatar className src={avatar} alt="reactAvatar"/>  
+
+        </Box>
+        <Box component="nav">
            <AppBar position="static" style={{background: "#222"}}>
                <Toolbar>
                    <IconButton>
                    <ArrowBack style={{color: "red"}}/>
                    </IconButton>
+                   <Typography variant="h5" style={{ color: "tan"}}>
+                      Portfolio
+                   </Typography>
                </Toolbar>
            </AppBar>
         </Box>
+        </>
     )
 };
 
