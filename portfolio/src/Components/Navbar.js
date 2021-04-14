d3a0c26af60ec33ca5b6423b29e775554d905d0a
David Ready <davis_ready@hotmail.com>
2021-04-14 12:35:33 -0400

    fixed background color issue

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,57 +1,89 @@
-
-import React from 'react'
-import { AppBar,  
-         Toolbar, 
-         ListItem, 
-         IconButton, 
-         ListItemText, 
-         Avatar, 
-         Divider, 
-         List, 
-         Typography, 
-         Box 
-        } from '@material-ui/core';
-import { ArrowBack, 
-         AssignmentInd, 
-         Home, 
-         Apps, 
-         ContactMail 
-        } from '@material-ui/icons';
-import avatar from "../avatar.png"
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { 
+    AppBar,  
+    Toolbar, 
+    ListItem, 
+    IconButton,
+    ListItemIcon, 
+    ListItemText, 
+    Avatar, 
+    Divider, 
+    List, 
+    Typography, 
+    Box 
+} from '@material-ui/core';
+import { 
+    ArrowBack, 
+    AssignmentInd, 
+    Home,
+    ContactMail, 
+    Apps,  
+} from '@material-ui/icons';
+import avatar from "../avatar.png";
 
 
 //CSS STYLES
-const useStyles = makeStyles(theme=>({
+const useStyles = makeStyles({
     menuSliderContainer: {
         width: 250,
         background: "#511",
         height: "30rem"
-    },
-    avatar: {
-        display: "block",
-        margin: "0.5rem auto",
-        width: theme.spacing(13),
-        height: theme.spacing(13)
     }
-}));
+})
+    //avatar: {
+    //    display: "block",
+    //    margin: "0.5rem auto",
+    //    width: theme.spacing(13),
+    //    height: theme.spacing(13)
+    //}
+
+const menuItems = [
+    {
+        listIcon: <Home/>,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd/>,
+        listText: "Resume"
+    },
+    {
+        listIcon: <Apps/>,
+        listText: "Portfolio"
+    },
+    {
+        listIcon: <ContactMail/>,
+        listText: "Contacts"
+    },
+]
 
 const Navbar = () => {
-    const classes = useStyles;
+    const classes = useStyles();
     return (
         <>
         <Box className={classes.menuSliderContainer} component="div">
-          <Avatar className src={avatar} alt="reactAvatar"/>  
+          <Avatar className={classes.avatar} src={avatar} alt="reactAvatar" />  
+          <Divider />
+          <List>
+              {menuItems.map((listItem, key)=>(
 
+                <ListItem button>
+                <ListItemIcon>
+
+                </ListItemIcon>
+                <ListItemText/>
+                
+              </ListItem>
+            ))}
+          </List>
         </Box>
         <Box component="nav">
-           <AppBar position="static" style={{background: "#222"}}>
+           <AppBar position="static" style={{ background: "#222" }}>
                <Toolbar>
                    <IconButton>
-                   <ArrowBack style={{color: "red"}}/>
+                   <ArrowBack style={{ color: "tomato" }} />
                    </IconButton>
-                   <Typography variant="h5" style={{ color: "tan"}}>
+                   <Typography variant="h5" style={{ color: "tan" }}>
                       Portfolio
                    </Typography>
                </Toolbar>
