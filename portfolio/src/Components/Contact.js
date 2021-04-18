c83d270e340e911249710e7d8717db7660c9d91b
David Ready <davis_ready@hotmail.com>
2021-04-18 12:21:51 -0400

    created Contact form

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -6,10 +6,11 @@ import Navbar from './Navbar.js';
 
 
 
+
 const InputField = withStyles({
     root: {
         "& label.Mui-focused": {
-            color: "tomato",
+            color: "tan",
         },
         "& label": {
             color: "tan",
@@ -18,11 +19,29 @@ const InputField = withStyles({
             "& fieldset": {
             borderColor: "tan",
         },
+        "&:hover fieldset": {
+            borderColor: "tan",
+        },
+        "& .Mui-focused field": {
+            borderColor: "tan",
+        },
         },
     },
 })(TextField);
 
-const useStyles = makeStyles();
+const useStyles = makeStyles(theme=>({
+    form: {
+        top: "35%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        position: "absolute",
+    },
+    button: {
+        marginTop: "1rem",
+        color: "tomato",
+        borderColor: "tomato",
+    },
+}));
 
 const Contact = () => {
 
@@ -31,17 +50,47 @@ const classes = useStyles();
     return (
         <>
         <Navbar />
-        <Box component="div" variant="h1">
+        <Box component="div" style={{ background: "#233", height: "100vh"}}>
             <Grid container justify="center">
-                <Box component="form">    
-            <Typography variant="h5">
-               Hire or Contact Me!
+                <Box component="form" className={classes.form}>    
+            <Typography variant="h5" style={{color: "tomato", textAlign: "center", textTransform: "uppercase"}}>
+               Hire or Contact Me...
             </Typography>
             <InputField
             fullWidth={true}
             label="Name"
             variant="outlined"
+            inputProps={{style:{ color: "white" } }} 
+            margin="dense"
+            size="medium"
             />
+            <br />
+             <InputField
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+            inputProps={{style:{ color: "white"}}} 
+            margin="dense"
+            size="medium"
+            />
+            <br />
+             <InputField
+            fullWidth={true}
+            label="Company Name"
+            variant="outlined"
+            inputProps={{style:{ color: "white"}}} 
+            margin="dense"
+            size="medium"
+            />
+            <br />
+            <Button 
+            className={classes.button} 
+            variant="outlined" 
+            fullWidth={true} 
+            endIcon={<SendIcon />}
+            >
+                contact me
+            </Button>
             </Box>
             </Grid>
         </Box>
