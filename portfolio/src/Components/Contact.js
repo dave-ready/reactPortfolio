2dcc085cd2027c516b880e6200458fd4b789781f
David Ready <davis_ready@hotmail.com>
2021-04-18 00:42:28 -0400

    'Name' input field

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -1,19 +1,49 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
-import Navbar from './Navbar.js'
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, FormControl, Button } from "@material-ui/core";
+import React from 'react';
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { TextField, Typography, Grid, Box, Button } from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
+import Navbar from './Navbar.js';
 
+
+
+const InputField = withStyles({
+    root: {
+        "& label.Mui-focused": {
+            color: "tomato",
+        },
+        "& label": {
+            color: "tan",
+        },
+        "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+            borderColor: "tan",
+        },
+        },
+    },
+})(TextField);
+
+const useStyles = makeStyles();
 
 const Contact = () => {
+
+const classes = useStyles();   
 
     return (
         <>
         <Navbar />
-        <Box component="header" variant="h1">
-            <Typography>
-                CONTACT COMPONENT
+        <Box component="div" variant="h1">
+            <Grid container justify="center">
+                <Box component="form">    
+            <Typography variant="h5">
+               Hire or Contact Me!
             </Typography>
+            <InputField
+            fullWidth={true}
+            label="Name"
+            variant="outlined"
+            />
+            </Box>
+            </Grid>
         </Box>
         </>
     )   
